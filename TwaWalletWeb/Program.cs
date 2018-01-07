5359ef28d4c34c47f1fadee2b1ca8a9a8c3eab17
Tomáš Wagner <t.e.n.k@seznam.cz>
2018-01-07 22:35:05 +0100

    MVC and HomeController added

--- TwaWalletWeb/Program.cs
+++ TwaWalletWeb/Program.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace TwaWalletWeb
 {
     public class Program
-    {
+    {       
+
         public static void Main(string[] args)
         {
+            //BuildWebHost(args).Run();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -21,5 +27,10 @@ namespace TwaWalletWeb
 
             host.Run();
         }
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
     }
 }
--- TwaWalletWeb/Startup.cs
+++ TwaWalletWeb/Startup.cs
@@ -6,20 +6,26 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Configuration;
 
 namespace TwaWalletWeb
 {
     public class Startup
     {
-
-        public IConfigurationRoot Configuration { get; }
+        //public static IConfigurationRoot Configuration { get; set; }
+        public IConfiguration Configuration { get; set; }
 
         public Startup()
         {
             var builder = new ConfigurationBuilder()
-            .AddJsonFile("AppSettings.json");
+            .AddJsonFile("AppSettings.json");            
             Configuration = builder.Build();
         }
 
@@ -27,29 +33,35 @@ namespace TwaWalletWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+            //app.UseIISPlatformHandler(); // kontrola windows authentikace (identity)
+            //app.UseWelcomePage();            
+            //app.UseRuntimeInfoPage();
+            app.UseDeveloperExceptionPage();
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
+            app.UseFileServer(); // UseDefaultFiles + UseStaticFiles in appropriate order
+            app.UseMvcWithDefaultRoute();
 
-            //app.UseIISPlatformHandler();
-            app.Run(async (context) => 
+            app.Run(async (context) =>
             {
+                //throw new InvalidOperationException("Něco se pokazilo... :(");
                 var msg = Configuration["message"];
                 await context.Response.WriteAsync(msg);
             });
         }
+
+        // Entry point for the application. 
+        //public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
