36006159cc2fa8f89dce4048a5534b250919b7bb
Tomáš Wagner <t.e.n.k@seznam.cz>
2018-01-07 01:15:39 +0100

    Startup.cs and AppSettings.json

--- TwaWalletWeb/Startup.cs
+++ TwaWalletWeb/Startup.cs
@@ -7,11 +7,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace TwaWalletWeb
 {
     public class Startup
     {
+
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile("AppSettings.json");
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,9 +39,16 @@ namespace TwaWalletWeb
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            //app.UseIISPlatformHandler();
+            app.Run(async (context) => 
             {
-                await context.Response.WriteAsync("Hello World!");
+                var msg = Configuration["message"];
+                await context.Response.WriteAsync(msg);
             });
         }
     }
